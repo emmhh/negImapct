4c6904a626d79e11cc67ef87b41b45115edf7d30
Marvin <41564670+emmhh@users.noreply.github.com>
2023-10-08 02:17:30 -0400

    fixed the animation of the cards

--- Assets/Scripts/CardDeck.cs
+++ Assets/Scripts/CardDeck.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Card : ScriptableObject
+public class CardDeck : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
