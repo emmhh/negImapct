9212ccffcc96d6183b77964eee14b43dc9dcbea8
Sophia Wang <44634224+w94090@users.noreply.github.com>
2023-10-08 03:27:02 -0400

    Add a bit of code in floorTrigger

--- Assets/Scripts/floorTrigger.cs
+++ Assets/Scripts/floorTrigger.cs
@@ -3,6 +3,9 @@ using UnityEngine.SceneManagement;
 
 public class floorTrigger : MonoBehaviour
 {
+    // Store the respawn position
+    //private Vector3 respawnPosition = new Vector3(7.49737453f, 1.82008123f, 9.57754993f);
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
@@ -15,5 +18,11 @@ public class floorTrigger : MonoBehaviour
     {
         int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
         SceneManager.LoadScene(currentSceneIndex);
+        // Set the player's position to the respawn position
+        //GameObject player = GameObject.FindGameObjectWithTag("Player");
+        //if (player != null)
+        //{
+        //   player.transform.position = respawnPosition;
+        // }
     }
 }
